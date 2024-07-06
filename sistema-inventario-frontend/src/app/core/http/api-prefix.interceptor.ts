3bf615a4a1be51262c020842df0e6bc485ad1ee9
centii123 <esy.mera@yavirac.edu.ec>
2024-07-06 10:59:43 -0500

    coneccion y dependencia jwt decode

---
+++ sistema-inventario-frontend/src/app/core/http/api-prefix.interceptor.ts
@@ -0,0 +1,53 @@
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { jwtDecode } from "jwt-decode";
+import { Observable } from "rxjs";
+
+@Injectable({
+    providedIn: 'root'
+  })
+  export class ApiService {
+    private apiUrl: any;
+  
+    constructor(private http: HttpClient) {
+      this.apiUrl = 'http://localhost:8080'
+  
+    }
+  
+    get(ruta:string): Observable<any[]> {
+      if(localStorage.getItem('token')){
+        const headers = new HttpHeaders({
+          'Authorization': `${localStorage.getItem('token')}`
+        });
+        return this.http.get<any[]>(`${this.apiUrl}/${ruta}`,{headers});
+      }
+      return this.http.get<any[]>(`${this.apiUrl}/${ruta}`);
+    }
+  
+    getUserName(username:string,token:string){
+      const headers = new HttpHeaders({
+        'Authorization': `${token}`
+      });
+      console.log(headers)
+      return this.http.post<any[]>(`${this.apiUrl}/api/user/user/`,{username:username}, { headers });
+    }
+  
+    async login(username: string, password: string) {
+      const fecher = async (url: string, token: string) => {
+        return await fetch(url, {
+          method: 'POST',
+          headers: {
+            'Authorization': token
+          }
+        })
+      }
+      const token = 'Basic ' + btoa(`${username}:${password}`);
+      const response = await fecher(this.apiUrl + '/login', token);
+      if (response.ok) {
+        let bearerToken = response.headers.get("Authorization") || "";
+        localStorage.setItem("token",bearerToken)
+        const decoded = jwtDecode(bearerToken);
+        //console.log(decoded)
+      }
+    }
+}
