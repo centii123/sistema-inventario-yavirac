e5433ba7b7687a757d0af0f72d8bf1e131ca9def
JohanP151 <jas.pavon@yavirac.edu.ec>
2024-09-05 14:57:44 -0500

    cambios codigo basura

--- sistema-inventario-frontend/src/app/modules/principal/principal-routing.module.ts
+++ sistema-inventario-frontend/src/app/modules/principal/principal-routing.module.ts
@@ -3,15 +3,7 @@ import { Routes, RouterModule } from '@angular/router';
 
 // Importa los componentes que necesitas para las rutas
 import { DocentesComponent } from './docentes/docentes.component';
-import { DocentesInfoComponent } from './docentes-info/docentes-info.component';
 import { InventarioComponent } from './inventario/inventario.component';
-import { InventarioAulasComponent } from './inventario-aulas/inventario-aulas.component';
-import { InventarioInfoAulasComponent } from './inventario-info-aulas/inventario-info-aulas.component';
-import { InventarioInfoLaboratorioComponent } from './inventario-info-laboratorio/inventario-info-laboratorio.component';
-import { InventarioInfoOficinasComponent } from './inventario-info-oficinas/inventario-info-oficinas.component';
-import { InventarioLaboratoriosComponent } from './inventario-laboratorios/inventario-laboratorios.component';
-import { InventarioOficinasComponent } from './inventario-oficinas/inventario-oficinas.component';
-import { DocentesNewComponent } from './docentes-new/docentes-new.component';
 import { FormComponent } from './componentes/nacionalidad/form/form.component';
 
 
@@ -33,16 +25,7 @@ const routes: Routes = [
   { path: 'generos', loadChildren: () => import('./componentes/genero/genero.module').then(m => m.GeneroModule) },
   { path: 'docentes', pathMatch: 'full', redirectTo: 'docentes' },
   { path: 'docentes', component: DocentesComponent },
-  { path: 'docentes-info', component: DocentesInfoComponent },
-  { path: 'docentes-new', component: DocentesNewComponent },
   { path: 'inventario', component: InventarioComponent, data: { sidebarContent: 'inventario' } },
-  { path: 'inventario-aulas', component: InventarioAulasComponent, data: { sidebarContent: 'inventario' } },
-  { path: 'inventario-info-aulas', component: InventarioInfoAulasComponent, data: { sidebarContent: 'inventario' } },
-  { path: 'inventario-info-laboratorio', component: InventarioInfoLaboratorioComponent, data: { sidebarContent: 'inventario' } },
-  { path: 'inventario-info-oficinas', component: InventarioInfoOficinasComponent, data: { sidebarContent: 'inventario' } },
-  { path: 'inventario-laboratorios', component: InventarioLaboratoriosComponent, data: { sidebarContent: 'inventario' } },
-  { path: 'inventario-oficinas', component: InventarioOficinasComponent, data: { sidebarContent: 'inventario' } },
-  { path: 'nacionalidad-table', component: InventarioLaboratoriosComponent },
   { path: 'nacionalidad', component: FormComponent },
 
   // Puedes añadir más rutas según necesites
--- sistema-inventario-frontend/src/app/modules/principal/principal.module.ts
+++ sistema-inventario-frontend/src/app/modules/principal/principal.module.ts
@@ -2,15 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PrincipalRoutingModule } from './principal-routing.module';
 import { DocentesComponent } from './docentes/docentes.component';
-import { DocentesInfoComponent } from './docentes-info/docentes-info.component';
-import { DocentesNewComponent } from './docentes-new/docentes-new.component';
 import { InventarioComponent } from './inventario/inventario.component';
-import { InventarioAulasComponent } from './inventario-aulas/inventario-aulas.component';
-import { InventarioInfoAulasComponent } from './inventario-info-aulas/inventario-info-aulas.component';
-import { InventarioInfoLaboratorioComponent } from './inventario-info-laboratorio/inventario-info-laboratorio.component';
-import { InventarioInfoOficinasComponent } from './inventario-info-oficinas/inventario-info-oficinas.component';
-import { InventarioLaboratoriosComponent } from './inventario-laboratorios/inventario-laboratorios.component';
-import { InventarioOficinasComponent } from './inventario-oficinas/inventario-oficinas.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { SharedModule } from "../../shared/shared.module";
 // Importaciones de PrimeNG
@@ -34,15 +26,7 @@ import { GlobalConfirmDialogModule } from "../../shared/global-confirm-dialog/gl
 @NgModule({
   declarations: [
     DocentesComponent,
-    DocentesInfoComponent,
-    DocentesNewComponent,
     InventarioComponent,
-    InventarioAulasComponent,
-    InventarioInfoAulasComponent,
-    InventarioInfoLaboratorioComponent,
-    InventarioInfoOficinasComponent,
-    InventarioLaboratoriosComponent,
-    InventarioOficinasComponent,
     TableComponent,
     FormComponent
   ],
